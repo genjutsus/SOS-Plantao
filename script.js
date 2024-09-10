2bfbfb3585b21d1e2c809316290f08666984fc31
genjutsus <154602378+genjutsus@users.noreply.github.com>
2024-09-10 13:48:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,76 +13,76 @@ document.addEventListener('DOMContentLoaded', () => {
                 <input type="text" id="input1" name="input1" required><br>
                 
                 <label for="input2">Nome do Funcionário comunicante:</label>
-                <input type="number" id="input2" name="input2" required><br>
+                <input type="text" id="input2" name="input2" required><br>
                 
                 <label for="input3">CPF do Funcionário:</label>
-                <input type="number" id="input3" name="input3" required><br>
+                <input type="text" id="input3" name="input3" required><br>
                 
                 <label for="input4">Nome do Falecido:</label>
-                <input type="number" id="input4" name="input4" required><br>
+                <input type="text" id="input4" name="input4" required><br>
                 
                 <label for="input5">Nacionalidade do Falecido:</label>
-                <input type="number" id="input5" name="input5" required><br>
+                <input type="text" id="input5" name="input5" required><br>
                 
                 <label for="input6">Falecido é natural de qual cidade?</label>
-                <input type="number" id="input6" name="input6" required><br>
+                <input type="text" id="input6" name="input6" required><br>
                 
                 <label for="input7">Estado Civil do Falecido:</label>
-                <input type="number" id="input7" name="input7" required><br>
+                <input type="text" id="input7" name="input7" required><br>
                 
                 <label for="input8">Data do Nascimento do Falecido:</label>
-                <input type="number" id="input8" name="input8" required><br>
+                <input type="text" id="input8" name="input8" required><br>
                 
                 <label for="input9">Identidade (nº do doc/órgão) do Falecido:</label>
-                <input type="number" id="input9" name="input9" required><br>
+                <input type="text" id="input9" name="input9" required><br>
                 
                 <label for="input10">Endereço do Falecido:</label>
-                <input type="number" id="input10" name="input10" required><br>
+                <input type="text" id="input10" name="input10" required><br>
                 
                 <label for="input11">Data de Entrada no Hospital/Órgão solicitante:</label>
-                <input type="number" id="input11" name="input11" required><br>
+                <input type="text" id="input11" name="input11" required><br>
                 
                 <label for="input12">Hora de Entrada no Hospital/Órgão solicitante:</label>
-                <input type="number" id="input12" name="input12" required><br>
+                <input type="text" id="input12" name="input12" required><br>
                 
                 <label for="input13">Número do BAM:</label>
-                <input type="number" id="input13" name="input13" required><br>
+                <input type="text" id="input13" name="input13" required><br>
                 
                 <label for="input14">Número do Prontuário:</label>
-                <input type="number" id="input14" name="input14" required><br>
+                <input type="text" id="input14" name="input14" required><br>
                 
                 <label for="input15">Motivo da Morte (em tese):</label>
-                <input type="number" id="input15" name="input15" required><br>
+                <input type="text" id="input15" name="input15" required><br>
                 
                 <label for="input16">Local da Morte:</label>
-                <input type="number" id="input16" name="input16" required><br>
+                <input type="text" id="input16" name="input16" required><br>
                 
                 <label for="input17">Primeiro Hospital/Órgão em que o falecido deu entrada:</label>
-                <input type="number" id="input17" name="input17" required><br>
+                <input type="text" id="input17" name="input17" required><br>
                 
                 <label for="input18">Socorrido por? ("Meios próprios" ou "Nome, Documento (pessoa física)" ou "Ambulância, Identificação da Ambulância"</label>
-                <input type="number" id="input18" name="input18" required><br>
+                <input type="text" id="input18" name="input18" required><br>
                 
                 <label for="input19">Data da Morte:</label>
-                <input type="number" id="input19" name="input19" required><br>
+                <input type="text" id="input19" name="input19" required><br>
                 
                 <label for="input20">Horário da Morte:</label>
-                <input type="number" id="input20" name="input20" required><br>
+                <input type="text" id="input20" name="input20" required><br>
                 
                 <label for="input21">Nome do Médico que atestou a Morte:</label>
-                <input type="number" id="input21" name="input21" required><br>
+                <input type="text" id="input21" name="input21" required><br>
                 
                 <label for="input22">CRM do Médico que atestou a Morte:</label>
-                <input type="number" id="input22" name="input22" required><br>
+                <input type="text" id="input22" name="input22" required><br>
                 
                 <label for="input23">Número da Guia:</label>
-                <input type="number" id="input23" name="input23" required><br>
+                <input type="text" id="input23" name="input23" required><br>
                 
                 <label for="input24">Nome de quem identificou o Falecido:</label>
-                <input type="number" id="input24" name="input24" required><br>
+                <input type="text" id="input24" name="input24" required><br>
                 
                 <label for="input25">Parentesco entre quem identificou e o Falecido:</label>
-                <input type="number" id="input25" name="input25" required><br>
+                <input type="text" id="input25" name="input25" required><br>
                 
                 <input type="submit" value="Enviar">
             </form>
